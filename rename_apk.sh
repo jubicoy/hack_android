89b083755c8bbb7689a9619f10fca5ebe0fe642d
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2015-09-29 13:51:14 +0300

    Optionally ncluded git SHA into apk name

--- rename_apk.sh
+++ rename_apk.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "2 arguments required. Exiting ..."
-    echo "Usage: ./rename_apk.sh <name slug> <target apk in outputs/apk>"
+function usage () {
+    echo "Usage: ./rename_apk.sh <target apk in outputs/apk> <name slug> [<hash>]"
     exit 1
+}
+
+if [ "$#" -lt 2 ]; then
+    echo "2 arguments required. Exiting ..."
+    usage
+fi
+
+if [ "$#" -qt 3 ]; then
+    echo "Too many arguments. Exiting ..."
+    usage
 fi
 
 NAME_SLUG=${1}
 TARGET_APK=${2}
+HASH=${3:0:7}
 TIMESTAMP=$(date +"%Y%m%d")
 
 APK_DIR="app/build/outputs/apk"
 
-mv "${APK_DIR}/${TARGET_APK}" "${APK_DIR}/${NAME_SLUG}_${TIMESTAMP}.apk"
+mv "${APK_DIR}/${TARGET_APK}" "${APK_DIR}/${NAME_SLUG}-${TIMESTAMP}-${HASH}.apk"
