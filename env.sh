577e2ac892778ff1fd4f83d4de440938472e639d
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2016-01-12 09:44:42 +0200

    BUILD_DIR prefix to ANDROID_HOME sdk path

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-export ANDROID_HOME=".sdk/android-sdk-linux"
+export ANDROID_HOME="${BUILD_DIR:-$(pwd)}/.sdk/android-sdk-linux"
 export PATH="$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools:$PATH"
